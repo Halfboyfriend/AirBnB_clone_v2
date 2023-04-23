21b4f9e575828bbf5099485d984c60be0c14c021
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-04-23 21:10:01 +0100

    flask-run

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,15 +5,17 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+@app.route('/', strict_slashes=False)
+def home():
     return "Hello HBNB!"
+
+
 
 
 if __name__ == "__main__":
