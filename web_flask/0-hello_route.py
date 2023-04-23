019ea29f0ef0f0b105061f084bfc5bd4e25693d9
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-04-23 21:15:18 +0100

    flask-run

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,18 +5,15 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
-
 from flask import Flask
-
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def home():
-    """Displays Hello"""
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
-
-
 
 
 if __name__ == "__main__":
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,20 +8,19 @@ Routes:
 """
 from flask import Flask
 
-
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def home():
-    """Displays Hello"""
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def root():
-    """Displays HBNB"""
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
-
 
 
 if __name__ == "__main__":
